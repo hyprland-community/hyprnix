e116d0d76f36c81913e3642ea9c4bed743b72808
Jacob Birkett <spikespaz@outlook.com>
2023-09-16 00:00:06 -0700

    lib: types: extract configFile from hm-module

--- hm-module/default.nix
+++ hm-module/default.nix
@@ -8,6 +8,7 @@ let
 in let
   lib = lib';
   inherit (lib) types;
+  inherit (lib.hl.types) configFile;
 
   cfg = config.wayland.windowManager.hyprland;
 in {
@@ -24,69 +25,10 @@ in {
     (import ./keybinds.nix self)
   ];
 
-  options = let
-    configFile = basePath:
-      types.submodule ({ config, name, ... }: {
-        options = {
-          target = lib.mkOption {
-            type = types.singleLineStr;
-            readOnly = true;
-            description = lib.mdDoc ''
-              The path of which to write {option}`source` or {option}`text`.
-
-              This is always specified as the attribute name of this
-              object in its parent attribute set.
-            '';
-          };
-          source = lib.mkOption {
-            type = types.path;
-            default = null;
-            description = lib.mdDoc ''
-              If {option}`text` is not specified, this path will be used.
-
-              If this is a directory, contents will be linked recursively.
-            '';
-          };
-          text = lib.mkOption {
-            type = types.nullOr types.lines;
-            default = null;
-            description = lib.mdDoc ''
-              The text contents of the file.
-
-              This option can be set mutiple times,
-              and new text will be appended after previous lines.
-              Use `lib.mkOrder` to ensure lines are written
-              in the order you desire.
-
-              This option takes precedence over {option}`source`.
-            '';
-          };
-          executable = lib.mkOption {
-            type = types.bool;
-            default = false;
-            description = lib.mdDoc ''
-              If this file should be marked as executable.
-
-              Useful for scripts used by the Hyprland config itself,
-              for example keybinds using the `exec` dispatcher.
-
-              Only works if {option}`text` is set.
-            '';
-          };
-        };
-        config = {
-          target = name;
-          source = lib.mkIf (config.text != null) (pkgs.writeTextFile {
-            name = "${basePath}/${config.target}";
-            destination = "/${config.target}";
-            inherit (config) text executable;
-          });
-        };
-      });
-  in {
+  options = {
     wayland.windowManager.hyprland = {
       configFile = lib.mkOption {
-        type = types.attrsOf (configFile "${config.xdg.configHome}/hypr");
+        type = types.attrsOf (configFile pkgs "${config.xdg.configHome}/hypr");
         default = { };
         description = lib.mdDoc ''
           Configuration files and directories to link in the Hyprland config directory.
---
+++ lib/types.nix
@@ -0,0 +1,65 @@
+{ lib, ... }:
+let
+  inherit (lib) types;
+
+  configFile = pkgs: basePath:
+    types.submodule ({ config, name, ... }: {
+      options = {
+        target = lib.mkOption {
+          type = types.singleLineStr;
+          readOnly = true;
+          description = lib.mdDoc ''
+            The path of which to write {option}`source` or {option}`text`.
+
+            This is always specified as the attribute name of this
+            object in its parent attribute set.
+          '';
+        };
+        source = lib.mkOption {
+          type = types.path;
+          default = null;
+          description = lib.mdDoc ''
+            If {option}`text` is not specified, this path will be used.
+
+            If this is a directory, contents will be linked recursively.
+          '';
+        };
+        text = lib.mkOption {
+          type = types.nullOr types.lines;
+          default = null;
+          description = lib.mdDoc ''
+            The text contents of the file.
+
+            This option can be set mutiple times,
+            and new text will be appended after previous lines.
+            Use `lib.mkOrder` to ensure lines are written
+            in the order you desire.
+
+            This option takes precedence over {option}`source`.
+          '';
+        };
+        executable = lib.mkOption {
+          type = types.bool;
+          default = false;
+          description = lib.mdDoc ''
+            If this file should be marked as executable.
+
+            Useful for scripts used by the Hyprland config itself,
+            for example keybinds using the `exec` dispatcher.
+
+            Only works if {option}`text` is set.
+          '';
+        };
+      };
+      config = {
+        target = name;
+        source = lib.mkIf (config.text != null) (pkgs.writeTextFile {
+          name = "${basePath}/${config.target}";
+          destination = "/${config.target}";
+          inherit (config) text executable;
+        });
+      };
+    });
+in { # #
+  inherit configFile;
+}
