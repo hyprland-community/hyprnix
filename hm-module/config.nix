fb55a5b90aeff7e43d060682a65eca0aa93ed53b
Jacob Birkett <spikespaz@outlook.com>
2023-09-28 22:47:39 -0700

    hm-mod: move systemdIntegration to environment.nix

--- hm-module/config.nix
+++ hm-module/config.nix
@@ -57,21 +57,6 @@ in {
       #   '';
       # };
 
-      systemdIntegration = lib.mkOption {
-        type = types.bool;
-        default = pkgs.stdenv.isLinux;
-        description = lib.mdDoc ''
-          Whether to enable {file}`hyprland-session.target` on
-          hyprland startup. This links to {file}`graphical-session.target`.
-          Some important environment variables will be imported to systemd
-          and dbus user environment before reaching the target, including:
-          - {env}`DISPLAY`
-          - {env}`HYPRLAND_INSTANCE_SIGNATURE`
-          - {env}`WAYLAND_DISPLAY`
-          - {env}`XDG_CURRENT_DESKTOP`
-        '';
-      };
-
       xwayland.enable = lib.mkOption {
         type = types.bool;
         default = true;
@@ -244,27 +229,6 @@ in {
     in {
       home.packages = lib.optional (cfg.package != null) package'
         ++ lib.optional cfg.xwayland.enable pkgs.xwayland;
-    })
-    (lib.mkIf cfg.systemdIntegration {
-      systemd.user.targets.hyprland-session = {
-        Unit = {
-          Description = "hyprland compositor session";
-          Documentation = [ "man:systemd.special(7)" ];
-          BindsTo = [ "graphical-session.target" ];
-          Wants = [ "graphical-session-pre.target" ];
-          After = [ "graphical-session-pre.target" ];
-        };
-      };
-
-      wayland.windowManager.hyprland.config.exec_once = [
-        "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP"
-        "systemctl --user start hyprland-session.target"
-      ];
-    })
-    (lib.mkIf (!cfg.systemdIntegration) {
-      wayland.windowManager.hyprland.config.exec_once = [
-        "${pkgs.dbus}/bin/dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP"
-      ];
     })
     (lib.mkIf (cfg.config != null) {
       wayland.windowManager.hyprland.configFile."hyprland.conf".text =
--- hm-module/environment.nix
+++ hm-module/environment.nix
@@ -8,6 +8,21 @@ let
 in {
   options = {
     wayland.windowManager.hyprland = {
+      systemdIntegration = lib.mkOption {
+        type = types.bool;
+        default = pkgs.stdenv.isLinux;
+        description = lib.mdDoc ''
+          Whether to enable {file}`hyprland-session.target` on
+          hyprland startup. This links to {file}`graphical-session.target`.
+          Some important environment variables will be imported to systemd
+          and dbus user environment before reaching the target, including:
+          - {env}`DISPLAY`
+          - {env}`HYPRLAND_INSTANCE_SIGNATURE`
+          - {env}`WAYLAND_DISPLAY`
+          - {env}`XDG_CURRENT_DESKTOP`
+        '';
+      };
+
       recommendedEnvironment = lib.mkOption {
         type = types.bool;
         default = pkgs.stdenv.isLinux;
@@ -18,7 +33,30 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.recommendedEnvironment {
-    home.sessionVariables = { NIXOS_OZONE_WL = "1"; };
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.systemdIntegration {
+      systemd.user.targets.hyprland-session = {
+        Unit = {
+          Description = "hyprland compositor session";
+          Documentation = [ "man:systemd.special(7)" ];
+          BindsTo = [ "graphical-session.target" ];
+          Wants = [ "graphical-session-pre.target" ];
+          After = [ "graphical-session-pre.target" ];
+        };
+      };
+
+      wayland.windowManager.hyprland.config.exec_once = [
+        "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP"
+        "systemctl --user start hyprland-session.target"
+      ];
+    })
+    (lib.mkIf (!cfg.systemdIntegration) {
+      wayland.windowManager.hyprland.config.exec_once = [
+        "${pkgs.dbus}/bin/dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP"
+      ];
+    })
+    (lib.mkIf cfg.recommendedEnvironment {
+      home.sessionVariables = { NIXOS_OZONE_WL = "1"; };
+    })
+  ];
 }
