71ff4421181e59c2fd3c0adc10d7569fe1812ca1
Jacob Birkett <jacob@birkett.dev>
2023-11-29 02:00:17 -0700

    hm-mod: monitors: replace x,y with pos, width,height with size

--- hm-module/monitors.nix
+++ hm-module/monitors.nix
@@ -5,6 +5,22 @@ let
   inherit (lib) types;
 
   cfg = config.wayland.windowManager.hyprland.monitors;
+
+  point2DType = numType:
+    types.submodule {
+      options = {
+        x = lib.mkOption {
+          type = numType;
+          description =
+            "The X-coordinate of a point, or the width of a rectangle.";
+        };
+        y = lib.mkOption {
+          type = numType;
+          description =
+            "The Y-coordinate of a point, or the height of a rectangle.";
+        };
+      };
+    };
 in {
   options = {
     wayland.windowManager.hyprland.monitors = lib.mkOption {
@@ -17,49 +33,26 @@ in {
               `hyprctl monitors`, for example `eDP-1` or `HDMI-A-1`.
             '';
           };
-          x = lib.mkOption {
-            type = types.nullOr types.ints.unsigned;
-            default = null;
-            description = ''
-              The horizontal position of this monitor, offset
-              from the top-left of virtual screen space.
-            '';
-          };
-          y = lib.mkOption {
-            type = types.nullOr types.ints.unsigned;
-            default = null;
-            description = ''
-              The vertical position of this monitor, offset
-              from the top-left of virtual screen space.
-            '';
-          };
-          position = lib.mkOption {
-            type = types.singleLineStr;
+          pos = lib.mkOption {
+            type = types.either (point2DType types.ints.unsigned)
+              (types.enum [ "auto" ]);
             default = "auto";
             description = ''
-              The position of the monitor, as a string
-              (Hyprland keyword value parameter).
-            '';
-          };
-          width = lib.mkOption {
-            type = types.nullOr types.ints.positive;
-            default = null;
-            description = ''
-              The physical width of the display, in pixels.
-            '';
-          };
-          height = lib.mkOption {
-            type = types.nullOr types.ints.positive;
-            default = null;
-            description = ''
-              The physical height of the display, in pixels.
+              The position of the monitor as `{ x, y }` attributes.
+
+              This is not the same as {option}`position` which is a
+              string or an enum string.
             '';
           };
           size = lib.mkOption {
-            type = types.singleLineStr;
+            type = types.either (point2DType types.ints.positive)
+              (types.enum [ "preferred" "highrr" "highres" ]);
             default = "preferred";
             description = ''
-              The physical size of the display, as a string, to pass
+              The physical size of the display as `{ x, y }` attributes.
+
+              This is not the same as {option}`option` which is a
+              string or an enum string.
             '';
           };
           scale = lib.mkOption {
@@ -72,16 +65,39 @@ in {
               size of 2880x1800 pixels would be 1920x1200 virtual pixels.
             '';
           };
+          position = lib.mkOption {
+            type = types.singleLineStr;
+            internal = true;
+            readOnly = true;
+          };
+          resolution = lib.mkOption {
+            type = types.singleLineStr;
+            internal = true;
+            readOnly = true;
+          };
         };
 
-        config = {
-          position = lib.mkIf (config.x != null && config.y != null)
-            (lib.mkDefault "${toString config.x}x${toString config.y}");
-          size = lib.mkIf (config.width != null && config.height != null)
-            (lib.mkDefault
-              "${toString config.width}x${toString config.height}");
+        config = let
+          posIsPoint = (point2DType types.ints.unsigned).check config.pos;
+          sizeIsPoint = (point2DType types.ints.positive).check config.size;
+        in {
+          position = lib.mkMerge [
+            # is X,Y
+            (lib.mkIf posIsPoint
+              "${toString config.pos.x}x${toString config.pos.y}")
+            # is enum string
+            (lib.mkIf (!posIsPoint) config.pos)
+          ];
+          resolution = lib.mkMerge [
+            # is X,Y
+            (lib.mkIf sizeIsPoint
+              "${toString config.size.x}x${toString config.size.y}")
+            # is enum string
+            (lib.mkIf (!sizeIsPoint) config.size)
+          ];
         };
       }));
+
       description = ''
         Monitors to configure. The attribute name is not used in the
         Hyprland configuration, but is a convenience for recursive Nix.
@@ -90,17 +106,19 @@ in {
         is specified in the `name` attribute for the monitor.
         It is not the attribute name of the monitor in *this* parent set.
       '';
+
       example = lib.literalExpression ''
         (with config.wayland.windowManager.hyprland.monitors; {
           # The attribute name `internal` is for usage in recursive Nix.
           internal = {
             name = "eDP-1";
-            position = "auto"; # `auto` is default
+            pos = "auto"; # `auto` is default
             size = "preferred"; # `preferred` is default
             bitdepth = 10;
           };
         })
       '';
+
       default = { };
     };
   };
@@ -108,12 +126,8 @@ in {
   config = {
     wayland.windowManager.hyprland.config.monitor = lib.mapAttrsToList
       (attrName:
-        { name, position, size, scale, bitdepth, ... }:
-        lib.concatStringsSep "," [
-          name
-          size
-          position
-          (toString scale)
-        ]) cfg;
+        { name, position, resolution, scale, ... }:
+        lib.concatStringsSep "," [ name position resolution (toString scale) ])
+      cfg;
   };
 }
