1defeb09a0a8b66ef5d60c2a0a86c83b4bfa46c1
Jacob Birkett <jacob@birkett.dev>
2024-06-11 02:58:11 -0700

    lib: gen: hyprlang: cut code from configFormat

--- lib/default.nix
+++ lib/default.nix
@@ -2,5 +2,10 @@ lib: lib0:
 lib0 // {
   hyprnix = lib0.hyprnix or { } // {
     types = lib0.hyprnix.types or { } // import ./hyprnix/types.nix lib lib0;
+    hyprlang = lib0.hyprnix.hyprlang or { }
+      // import ./hyprnix/hyprlang.nix lib lib0;
+  };
+  generators = lib0.generators or { } // {
+    toHyprlang = lib.hyprnix.hyprlang.toConfigString;
   };
 }
---
+++ lib/hyprnix/hyprlang.nix
@@ -0,0 +1,180 @@
+lib: _:
+let
+  defaultFormatterOptions = {
+    sortPred = _: _: false;
+    indentChars = "    ";
+    spaceAroundEquals = true;
+    lineBreakPred = prev: next:
+      let
+        betweenDifferent = nodeType prev != nodeType next;
+        betweenRepeats = isRepeatNode prev && isRepeatNode next;
+        betweenSections = isSectionNode prev && isSectionNode next;
+      in prev != null
+      && (betweenDifferent || betweenRepeats || betweenSections);
+  };
+
+  toConfigString = opts:
+    let opts' = defaultFormatterOptions // opts;
+    in attrs:
+    lib.pipe attrs [
+      (attrsToNodeList [ ])
+      (formatNodeList opts')
+      (renderNodeList opts')
+    ];
+
+  toPrettyM = lib.generators.toPretty { multiline = true; };
+
+  formatNodeList = opts: nodes:
+    lib.pipe nodes [
+      (sortNodeListRecursive opts.sortPred)
+      (insertLineBreakNodesRecursive opts.lineBreakPred)
+      (insertIndentNodesRecursive opts.indentChars)
+    ];
+
+  renderNodeList = opts: nodes: lib.concatStrings (map (renderNode opts) nodes);
+
+  isNode = node: lib.isAttrs node && node ? _node_type;
+  isNodeType = type: node: isNode node && node._node_type == type;
+  isStringNode = isNodeType "string";
+  isIndentNode = isNodeType "indent";
+  isVariableNode = isNodeType "variable";
+  isRepeatNode = isNodeType "repeatBlock";
+  isSectionNode = isNodeType "configDocument";
+
+  mkNodeType = type: path: name: value: {
+    _node_type = type;
+    inherit name value;
+    path = path ++ [ name ];
+  };
+  mkStringNode = mkNodeType "string";
+  mkIndentNode = mkNodeType "indent";
+  mkVariableNode = mkNodeType "variable";
+  mkRepeatNode = mkNodeType "repeatBlock";
+  mkSectionNode = mkNodeType "configDocument";
+
+  nodeType = builtins.getAttr "_node_type";
+  mapValue = fn: node: node // { value = fn node.value; };
+
+  # concatListsSep = sep: lib.foldl' (a: b: a ++ [sep] ++ b) [];
+
+  attrsToNodeList = path: attrs:
+    let
+      variables = lib.pipe attrs [
+        (lib.filterAttrs (_: v: !(lib.isAttrs v || lib.isList v)))
+        (lib.mapAttrsToList (mkVariableNode path))
+      ];
+      repeats = lib.pipe attrs [
+        (lib.filterAttrs (_: lib.isList))
+        (lib.mapAttrsToList (name: values:
+          mkRepeatNode path name
+          (map (value: mkVariableNode path name value) values)))
+      ];
+      sections = lib.pipe attrs [
+        (lib.filterAttrs (_: lib.isAttrs))
+        (lib.mapAttrsToList (name: value:
+          mkSectionNode path name (attrsToNodeList (path ++ [ name ]) value)))
+      ];
+    in lib.concatLists [ variables repeats sections ];
+
+  sortNodeListRecursive = sortPred:
+    let
+      recurse = l:
+        lib.pipe l [
+          (map
+            (node: if isSectionNode node then mapValue recurse node else node))
+          (lib.sort (a: b: sortPred a.path b.path))
+        ];
+    in recurse;
+
+  insertLineBreakNodesRecursive = breakPred:
+    let
+      recurse = lib.foldl' (nodes: next:
+        let
+          prev = if nodes == [ ] then
+            null
+          else
+            builtins.elemAt nodes (builtins.length nodes - 1);
+          next' = if isRepeatNode next || isSectionNode next then
+            mapValue recurse next
+          else
+            next;
+          newline = mkStringNode next.path "newline" "\n";
+        in if breakPred prev next' then
+          nodes ++ [ newline ] ++ [ next' ]
+        else
+          nodes ++ [ next' ]) [ ];
+    in recurse;
+
+  insertIndentNodesRecursive = indentChars:
+    let
+      recurse = lib.foldl' (nodes: next:
+        let
+          level = builtins.length next.path - 1;
+          indent = mkIndentNode next.path "indent" level;
+        in if isVariableNode next then
+          nodes ++ [ indent ] ++ [ next ]
+        else if isRepeatNode next then
+          nodes ++ [ (mapValue recurse next) ]
+        else if isSectionNode next then
+          nodes ++ [ indent (mapValue (v: (recurse v) ++ [ indent ]) next) ]
+        else
+          nodes ++ [ next ]) [ ];
+    in recurse;
+
+  # Creates a string with chars repeated N times.
+  repeatChars = chars: level:
+    lib.concatStrings (map (_: chars) (lib.range 1 level));
+
+  renderNode = opts: node:
+    if isStringNode node then
+      node.value
+    else if isIndentNode node then
+      repeatChars opts.indentChars node.value
+    else if isVariableNode node then
+      let equals = if opts.spaceAroundEquals then " = " else "=";
+      in ''
+        ${node.name}${equals}${valueToString node.value}
+      ''
+    else if isRepeatNode node then
+      lib.concatStrings (map (renderNode opts) node.value)
+    else if isSectionNode node then ''
+      ${node.name} {
+      ${renderNodeList opts node.value}}
+    '' else
+      abort ''
+        value is not of any known node type:
+        ${toPrettyM node}
+      '';
+
+  # Converts a single value to a valid Hyprland config RHS
+  valueToString = value:
+    if value == null then
+      ""
+    else if lib.isBool value then
+      lib.boolToString value
+    else if lib.isInt value || lib.isFloat value then
+      toString value
+    else if lib.isString value then
+      value
+    else if lib.isList value then
+      lib.concatMapStringsSep " " valueToString value
+    else
+      abort ''
+        could not convert value of type '${
+          builtins.typeOf value
+        }' to config string:
+        ${toPrettyM value}
+      '';
+in {
+  inherit
+  # Transforms
+    toConfigString attrsToNodeList formatNodeList renderNodeList
+    insertLineBreakNodesRecursive insertIndentNodesRecursive
+    # Checks
+    nodeType isNode isNodeType isStringNode isIndentNode isVariableNode
+    isRepeatNode isSectionNode
+    # Node Factories
+    mkStringNode mkIndentNode mkVariableNode mkRepeatNode mkSectionNode
+    # Utilities
+    mapValue renderNode valueToString;
+}
