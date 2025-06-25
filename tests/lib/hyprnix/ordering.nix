c87794e928305d29fc1844dd10f59ae586c982c1
Jacob Birkett <jacob@birkett.dev>
2025-06-25 02:50:38 -0700

    tests: ordering: orderOfPath: add specificity test

--- tests/lib/hyprnix/ordering.nix
+++ tests/lib/hyprnix/ordering.nix
@@ -1,12 +1,22 @@
 { lib }:
 lib.bird.mkTestSuite {
   orderOfPath = let inherit (lib.hyprnix.ordering) orderOfPath;
-  in [ # #
+  in [
     {
       name = "trivial ordering";
       expr = let patterns = [ [ "a" ] [ "b" ] [ "c" ] ];
       in map (path: orderOfPath path patterns) [ [ "c" ] [ "b" ] [ "a" ] ];
       expect = [ 2 1 0 ];
+    }
+    {
+      name = "ordering by specificity";
+      expr = let patterns = [ [ "a" ".*" ] [ "a" "c" ] ];
+      in map (path: orderOfPath path patterns) [
+        [ "a" "c" ]
+        [ "a" "b" ]
+        [ "a" "a" ]
+      ];
+      expect = [ 1 0 0 ];
     }
   ];
 }
