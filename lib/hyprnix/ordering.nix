26a49f424e97e8132487b6491a5ccc7e0b64f4db
Jacob Birkett <jacob@birkett.dev>
2025-06-25 02:50:02 -0700

    lib: ordering: orderOfPath: adjust to respect specificity

--- lib/hyprnix/ordering.nix
+++ lib/hyprnix/ordering.nix
@@ -3,19 +3,21 @@ let
   inherit (lib.hyprnix.lists) elemsMatch;
 
   # Given a list of strings as `path` and a list of `patterns`,
-  # return the index of the first match.
+  # return the index of the first most-specific match.
   # If no match is found, the resulting order is one more than the length of
   # `patterns`, positioning it last.
   orderOfPath = path: patterns:
     let
-      recurse = i: patterns:
+      recurse = prior: index: patterns:
         if lib.length patterns == 0 then
-          i + 1 # no match found, order is last
-        else if elemsMatch (lib.head patterns) path then
-          i # a match has been found
+          if prior != null then prior else index + 1
+        else if lib.head patterns == path then
+          index
+        else if prior == null && elemsMatch (lib.head patterns) path then
+          recurse index (index + 1) (lib.tail patterns)
         else
-          recurse (i + 1) (lib.tail patterns);
-    in recurse 0 patterns;
+          recurse prior (index + 1) (lib.tail patterns);
+    in recurse null 0 patterns;
 in { # #
   inherit orderOfPath;
 }
